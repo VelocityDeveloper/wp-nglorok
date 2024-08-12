b4eddc1498ceae5f16595976ccee110ff549705f
aadiityaak <aadiityaak@gmail.com>
2024-08-12 09:33:30 +0700

    init

---
+++ includes/class-wp-nglorok-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://velocitydeveloper.com
+ * @since      1.0.0
+ *
+ * @package    Wp_Nglorok
+ * @subpackage Wp_Nglorok/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Nglorok
+ * @subpackage Wp_Nglorok/includes
+ * @author     Velocity Developer <bantuanvelocity@gmail.com>
+ */
+class Wp_Nglorok_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wp-nglorok-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://velocitydeveloper.com
+ * @since      1.0.0
+ *
+ * @package    Wp_Nglorok
+ * @subpackage Wp_Nglorok/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Nglorok
+ * @subpackage Wp_Nglorok/includes
+ * @author     Velocity Developer <bantuanvelocity@gmail.com>
+ */
+class Wp_Nglorok_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
